8a3964138739237340033f7dedc506be8bf82ccd
pablosingh <pablo22_4@hotmail.com>
2025-01-29 12:29:36 -0300

    Porcentajes

--- src/pages/Holdings.jsx
+++ src/pages/Holdings.jsx
@@ -8,45 +8,45 @@ import HeadTableHolding from "../components/HeadTableHolding.jsx";
 import { tertiaryColor, tertiaryHoverColor } from "../styles/colors.js";
 
 export default function Holdings() {
-    const arrayHoldings = useSelector((state) => state?.holdings.holdings);
-    const state = useSelector((state) => state);
-    const [showTable, setShowTable] = useState(true);
-    return (
-        <ContainerStyled>
-            <button onClick={() => console.log(arrayHoldings)}>Holds</button>
-            <button onClick={() => console.log(state)}>Estado</button>
-            <CardFiltersButtons />
-            <Btn onClick={() => setShowTable(!showTable)}>Mostrar Tabla</Btn>
-            {showTable ? (
-                <HeadTableHolding />
-            ) : (
-                arrayHoldings &&
-                arrayHoldings?.map((hold) => (
-                    <CardHolding ticker={hold} key={hold.id} />
-                ))
-            )}
-            <Total />
-        </ContainerStyled>
-    );
+  const arrayHoldings = useSelector((state) => state?.holdings.holdings);
+  const state = useSelector((state) => state);
+  const [showTable, setShowTable] = useState(true);
+  return (
+    <ContainerStyled>
+      {/* <button onClick={() => console.log(arrayHoldings)}>Holds</button>
+            <button onClick={() => console.log(state)}>Estado</button> */}
+      <CardFiltersButtons />
+      {/* <Btn onClick={() => setShowTable(!showTable)}>Mostrar Tabla</Btn> */}
+      {showTable ? (
+        <HeadTableHolding />
+      ) : (
+        arrayHoldings &&
+        arrayHoldings?.map((hold) => (
+          <CardHolding ticker={hold} key={hold.id} />
+        ))
+      )}
+      <Total />
+    </ContainerStyled>
+  );
 }
 
 const ContainerStyled = styled.div`
-    width: 100vw;
-    height: 100vh;
-    margin: 0em 0em;
-    padding: 0em 0em;
-    // background-color: red;
+  width: 100vw;
+  height: 100vh;
+  margin: 0em 0em;
+  padding: 0em 0em;
+  // background-color: red;
 `;
 
 const Btn = styled.button`
-    background-color: ${tertiaryColor};
-    color: white;
-    border-radius: 1em;
-    padding: 0.2em 1em;
-    margin: 0.3em;
-    transition: all 0.4s ease;
-    &:hover {
-        background-color: ${tertiaryHoverColor};
-        color: black;
-    }
+  background-color: ${tertiaryColor};
+  color: white;
+  border-radius: 1em;
+  padding: 0.2em 1em;
+  margin: 0.3em;
+  transition: all 0.4s ease;
+  &:hover {
+    background-color: ${tertiaryHoverColor};
+    color: black;
+  }
 `;
